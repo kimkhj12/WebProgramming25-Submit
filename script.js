4057eaabae91092e5d44a0216cef2b83d76f460a
kimkhj12 <93894416+kimkhj12@users.noreply.github.com>
2025-11-30 20:53:47 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelector(".slides");
+  const images = document.querySelectorAll(".slides a"); // a 태그로 감싼 이미지
+  let index = 0;
+
+  function showSlide() {
+    slides.style.transform = `translateY(-${index * 500}px)`;
+  }
+
+  function nextSlide() {
+    index++;
+    if (index >= images.length) {
+      index = 0; // 처음으로 돌아가기
+    }
+    showSlide();
+  }
+
+  // 3초마다 자동으로 다음 슬라이드
+  setInterval(nextSlide, 3000);
+
+});
