26c6e66ccbdfb133232b0ddcc284e7e7aa2f51ec
kimkhj12 <93894416+kimkhj12@users.noreply.github.com>
2025-12-01 19:03:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let index = 0;
 
   function showSlide() {
-    slides.style.transform = `translateY(-${index * 400}px)`;
+    slides.style.transform = `translateY(-${index * 300}px)`;
   }
 
   function nextSlide() {
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(nextSlide, 3000);
 
 });
+
 
 
 
